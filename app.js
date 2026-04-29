5f1886d6be7327eba9d90e55bec104da82d33605
Ranjithbaswaraj <ranjithbaswaraju10@gmail.com>
2026-04-29 11:38:32 +0530

    cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express")
 const app=express()
 const port=3100;
+const cors=require("cors")
 
 const { ConnectDB } = require("./db_configure/db");
 const UserRoutes=require("./Routes/userRoutes")
@@ -14,6 +15,10 @@ app.use("/apiComment",CommentRoutes)
 app.use("/apiPosts",PostRoutes)
 
 ConnectDB()
+
+app.use(cors({
+    origin:["https://esphere-blog-client-v3d3.vercel.app/"]
+}))
 app.listen(port,()=>{
     console.log(`server running at ${port}`)
 })
